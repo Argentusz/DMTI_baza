cc4eb4e5cc9ead650629709866a3f5577f4e6a4b
Nata <96002623+Natanus2011@users.noreply.github.com>
2022-03-27 21:50:52 +0300

    функции перевода целое в дробное и обратно

--- rational/rational.go
+++ rational/rational.go
@@ -7,12 +7,26 @@ import (
 
 // Rational Структура рационального числа
 type Rational struct {
-	Nominator   whole.Whole
-	Denominator natural.Natural
+	nominator   whole.Whole
+	denominator natural.Natural
 }
 
 // MakeR Метод для создания рационального числа
 func (r *Rational) MakeR(nom whole.Whole, den natural.Natural) {
-	r.Nominator = nom
-	r.Denominator = den
+	r.nominator = nom
+	r.denominator = den
+}
+
+func Z_to_q(x whole.Whole) *Rational {
+	var one []uint8
+	one = append(one, 1)
+	p := Rational{nominator: whole.Whole{Negative: false, Num: natural.Natural{Older: 0, Digits: one}}, denominator: natural.Natural{Older: uint32(len(x.Num.Digits)), Digits: x.Num.Digits}}
+	return &p
+}
+
+func G_to_z(x Rational) *whole.Whole {
+	var one []int
+	one = append(one, 1)
+	p := whole.Whole{Negative: false, Num: x.nominator.Num}
+	return &p
 }
