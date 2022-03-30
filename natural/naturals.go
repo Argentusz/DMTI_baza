32d7800fb5f6141d0489ce026555d8473f1d1762
hope <nadezdaturbina00511@gmail.com>
2022-03-30 08:36:43 +0300

    Проверка на ноль и изменения в сравнении

--- natural/naturals.go
+++ natural/naturals.go
@@ -19,32 +19,46 @@ func (n *Natural) MakeN(digits []uint8) {
 	n.Older = uint32(len(digits)) - 1
 }
 
-// Турбина Compare Сравнение натуральных чисел: 2 - если первое больше второго, 0, если равно, 1 иначе.
+// Compare Турбина Сравнение натуральных чисел: 2 - если первое больше второго, 0, если равно, 1 иначе.
 func Compare(a, b *Natural) int {
 	var i uint32
 	var flag, res int
 	flag = 0
 	switch {
-	case a.Older > b.Older:
+	case a.Older > b.Older: //если в одном числе больше разрядов, чем в другом, то оно больше
 		flag = 1
 		res = 2
 	case b.Older > a.Older:
 		flag = 1
 		res = 1
 	default:
-		for i = 0; i < a.Older; i++ {
+		for i = 0; i < a.Older; i++ { //сравниваем разряды чисел
 			switch {
-			case a.Digits[0] > b.Digits[0]:
+			case a.Digits[i] > b.Digits[i]:
 				flag = 1
 				res = 2
-			case b.Digits[0] > a.Digits[0]:
+				break
+			case b.Digits[i] > a.Digits[i]:
 				flag = 1
 				res = 1
+				break
 			}
 		}
 	}
 	if flag == 0 {
 		res = 0
+	}
+	return res
+}
+
+// CheckNull Турбина Проверка на ноль: если число не равно нулю, то «да» иначе «нет»
+func CheckNull(x *Natural) bool {
+	var res bool
+	res = false
+	if x.Older == 0 { //если в числе один разряд и он равен нулю, то число является нулем
+		if x.Digits[0] == 0 {
+			res = true
+		}
 	}
 	return res
 }
