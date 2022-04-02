4f8e7fd6927cf95db58579cabe35cc1fc6734973
Nata <96002623+Natanus2011@users.noreply.github.com>
2022-04-02 13:20:46 +0300

    Функции перевода целого числа в дробное и обратно

--- rational/rational.go
+++ rational/rational.go
@@ -7,26 +7,40 @@ import (
 
 // Rational Структура рационального числа
 type Rational struct {
-	nominator   whole.Whole
-	denominator natural.Natural
+	Nominator   whole.Whole
+	Denominator natural.Natural
 }
 
 // MakeR Метод для создания рационального числа
 func (r *Rational) MakeR(nom whole.Whole, den natural.Natural) {
-	r.nominator = nom
-	r.denominator = den
+	r.Nominator = nom
+	r.Denominator = den
 }
 
-func Z_to_q(x whole.Whole) *Rational {
+//Функия Грунской Натальи
+//Функция перевода целого в дробное
+
+func WholeToFractional(x whole.Whole) Rational {
 	var one []uint8
-	one = append(one, 1)
-	p := Rational{nominator: whole.Whole{Negative: false, Num: natural.Natural{Older: 0, Digits: one}}, denominator: natural.Natural{Older: uint32(len(x.Num.Digits)), Digits: x.Num.Digits}}
-	return &p
+	var p Rational
+	one = append(one, 1) //делаем знаменатель 1
+
+	p.Nominator = whole.Whole{Num: natural.Natural{Digits: x.Num.Digits, //в числитель переносим массив цифр и знак
+		Older: uint32(len(x.Num.Digits))},
+		Negative: x.Negative}
+
+	p.Denominator = natural.Natural{Digits: one, //в знаменатель переносим единичку
+		Older: 0}
+	return p
 }
 
-func G_to_z(x Rational) *whole.Whole {
-	var one []int
-	one = append(one, 1)
-	p := whole.Whole{Negative: false, Num: x.nominator.Num}
-	return &p
+//Функия Грунской Натальи
+//Функция перевода дробного числа в целое
+
+func FractionalToWhole(x Rational) whole.Whole {
+
+	p := whole.Whole{Num: x.Nominator.Num, //переносим только числитель т.к. знаменатель 1
+		Negative: x.Nominator.Negative} //переносим знак числителя
+
+	return p
 }
