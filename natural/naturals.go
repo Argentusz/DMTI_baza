4000b02fe11e5026b6c2d8b9cff24657e5b94411
icekomar <miha.komar@mail.ru>
2022-04-03 19:11:47 +0300

    Вычитание из первого большего натурального числа второго меньшего или равного(сделал за милану)

--- natural/naturals.go
+++ natural/naturals.go
@@ -52,3 +52,51 @@ func CheckNull(x Natural) bool {
 	}
 	return false
 }
+
+// Комаровский Subtraction Вычитание из первого большего натурального числа второго меньшего или равного( сделал за Милану)
+func Subtraction(x1, x2 Natural) Natural {
+	var a, b, res Natural
+	var i, j, k int
+	var mass []uint8
+	// опрределяем большее число
+	if Compare(x1, x2) == 0 {
+		mass = append(mass, 0)
+		res.MakeN(mass)
+		return res
+	} else if Compare(x1, x2) == 2 {
+		a = x1
+		b = x2
+	} else if Compare(x1, x2) == 1 {
+		a = x2
+		b = x1
+	}
+	i = int(a.Older)
+	j = int(b.Older)
+	for j > -1 {
+		switch {
+		case a.Digits[i]-b.Digits[j] < 0:
+			if a.Digits[i-1] != 0 {
+				a.Digits[i-1] = a.Digits[i-1] - 1
+			} else {
+				// для случая если идет много нулей
+				k = i
+				for a.Digits[i-1] == 0 {
+					k -= 1
+					a.Digits[i] = 9
+				}
+				a.Digits[i-1] = a.Digits[i-1] - 1
+			}
+			mass = append([]uint8{a.Digits[i] + 10 - b.Digits[j]}, mass...)
+		default:
+			mass = append([]uint8{a.Digits[i] - b.Digits[j]}, mass...)
+		}
+		i -= 1
+		j -= 1
+	}
+	for i > -1 {
+		mass = append([]uint8{a.Digits[i]}, mass...)
+		i -= 1
+	}
+	res.MakeN(mass)
+	return res
+}
